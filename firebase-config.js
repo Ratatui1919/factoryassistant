44e27c94a27f3cbae5873cc8241311a307177d59
Ratatui1919 <161364644+Ratatui1919@users.noreply.github.com>
2026-02-28 19:58:22 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,44 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  signOut,
+  onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { 
+  getFirestore, 
+  doc, 
+  setDoc, 
+  getDoc, 
+  updateDoc 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// Ваша конфигурация из Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBn6fbSjjlT8dM41ov_uHEYPIDUfFgijo",
+  authDomain: "vaillant-assistant.firebaseapp.com",
+  projectId: "vaillant-assistant",
+  storageBucket: "vaillant-assistant.firebasestorage.app",
+  messagingSenderId: "94125964883",
+  appId: "1:94125964883:web:972621f4bfeaf2ab6e10a6"
+};
+
+// Инициализация Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { 
+  auth, 
+  db,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc
+};
