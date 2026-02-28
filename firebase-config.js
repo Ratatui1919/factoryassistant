b96ed224dd1d9e20a4e35cdb93df9689545c26d5
Ratatui1919 <161364644+Ratatui1919@users.noreply.github.com>
2026-02-28 20:51:06 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -14,14 +14,14 @@ import {
   updateDoc 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// ИСПРАВЛЕННАЯ КОНФИГУРАЦИЯ
+// ВАША КОНФИГУРАЦИЯ ИЗ FIREBASE
 const firebaseConfig = {
-  apiKey: "AIzaSyBn6fbSjjlTt8dm41ov_uHEYPIDUfFgii",
+  apiKey: "AIzaSyBn6fbSjjLTt8dm41ov_uHEYPIDUfFgiio",
   authDomain: "vaillant-assistant.firebaseapp.com",
   projectId: "vaillant-assistant",
   storageBucket: "vaillant-assistant.firebasestorage.app",
   messagingSenderId: "94125964883",
-  appId: "1:94125964883:web:972621f4bfeaf2ab6e10a6"
+  appId: "1:94125964883:web:3d363c1ef033431a6e10a6"
 };
 
 const app = initializeApp(firebaseConfig);
