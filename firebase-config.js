af06d8727c684f75c64ed9d09e7612a4f03425dc
Ratatui1919 <161364644+Ratatui1919@users.noreply.github.com>
2026-02-28 21:11:03 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -11,15 +11,18 @@ import {
   doc, 
   setDoc, 
   getDoc, 
-  updateDoc 
+  updateDoc,
+  collection,
+  query,
+  where,
+  getDocs
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// ВАША КОНФИГУРАЦИЯ
 const firebaseConfig = {
   apiKey: "AIzaSyBn6fbSjjLTt8dm41ov_uHEYPIDUfFgiio",
   authDomain: "vaillant-assistant.firebaseapp.com",
   projectId: "vaillant-assistant",
-  storageBucket: "vaillant-assistant.firebasestorage.app",
+  storageBucket: "vaillant-assistant.appspot.com",  // Исправлено!
   messagingSenderId: "94125964883",
   appId: "1:94125964883:web:3d363c1ef033431a6e10a6"
 };
@@ -38,5 +41,9 @@ export {
   doc,
   setDoc,
   getDoc,
-  updateDoc
+  updateDoc,
+  collection,
+  query,
+  where,
+  getDocs
 };
