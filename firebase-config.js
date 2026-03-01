981e0816189bbbe6b1e899fa8f435f68b60a31dd
Ratatui1919 <161364644+Ratatui1919@users.noreply.github.com>
2026-03-01 01:41:27 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,4 @@
-// Минимальный firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBn6fbSjjLTt8dm41ov_uHEYPIDUfFgiio",
@@ -13,7 +10,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+console.log('✅ Firebase инициализирован');
+export default app;
