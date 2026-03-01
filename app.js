029a2322c237de739d8c67ea6856b09dd5dc96c1
Ratatui1919 <161364644+Ratatui1919@users.noreply.github.com>
2026-03-01 01:43:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+import { auth, db } from './firebase-config.js';
 
+console.log('App.js загружен');
+console.log('Auth:', auth);
+console.log('DB:', db);
+
+document.body.innerHTML = '<h1 style="color:green; text-align:center; margin-top:50px;">✅ App.js работает!</h1>';
