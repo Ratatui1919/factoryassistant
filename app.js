0207682c4b35ae002a073ea4914a42c517a4a4a8
Ratatui1919 <161364644+Ratatui1919@users.noreply.github.com>
2026-03-01 01:41:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
-import { auth, db } from './firebase-config.js';
 
-console.log('Firebase подключен:', auth, db);
-document.body.innerHTML = '<h1 style="color:green; text-align:center; margin-top:50px;">✅ Firebase работает!</h1>';
